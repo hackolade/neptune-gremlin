85c97ed2afd11662b4dae7291edcd0e05783dd6b
Alik Rakhmonov <alik.rahmonov@gmail.com>
2025-09-03 14:05:58 +0200

    HCK-12556: Technical name property in relationships with coupling and naming convention

--- forward_engineering/gremlinHelper.js
+++ forward_engineering/gremlinHelper.js
@@ -44,7 +44,7 @@ const generateVertices = (collections, jsonData) => {
 };
 
 const generateEdge = (from, to, relationship, edgeData) => {
-	const edgeName = transformToValidGremlinName(relationship.name);
+	const edgeName = transformToValidGremlinName(relationship.code || relationship.name);
 	const propertiesScript = addPropertiesScript(relationship, edgeData, { cardinality: false });
 
 	return `${graphName}.addE(${JSON.stringify(edgeName)}).\n${DEFAULT_INDENT}from(${from}).\n${DEFAULT_INDENT}to(${to})${propertiesScript}`;
